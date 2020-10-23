b0a506026c7cf8ffda63c3ca99b71005f86a6dd5
Mikhail Chekov <chekoff@list.ru>
2020-10-23 15:44:49 +0300

    Added scroll to top btn

---
+++ src/js/ScrollTopBtn.js
@@ -0,0 +1,24 @@
+/**
+ * * Usage: ScrollTopBtn(elem)
+ * elem - button, which should srcroll to top of the page.
+*/
+
+export default function ScrollTopBtn (elem) {
+    elem.addEventListener('click', () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    window.addEventListener('scroll', scrollTopCheck);
+
+    function scrollTopCheck () {
+        if(window.pageYOffset > 300) {
+            elem.style.display = 'block';
+        } else {
+            elem.style.display = 'none';
+        }
+    }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -8,32 +8,40 @@ import TypingText from './TypingText';
 import SmoothScroll from './SmoothScroll';
 import FilterItems from './FilterItems';
 import AnimateProgressBar from './AnimateProgressBar';
+import ScrollTopBtn from './ScrollTopBtn';
 
 
 (function () {
     document.addEventListener("DOMContentLoaded", ready);
 
     function ready() {
+        //Copy 'src' from img tag to background of a parent elem.
         Ibg();
-
+        
         let typeTextElem = document.querySelector('.main-block__title');
         TypingText(typeTextElem ,['Привет, я Чех', 'ков Михаил'], [0, 1]);
 
         let readMoreELems = document.querySelectorAll('.item__descr');
         ToggleReadMore(readMoreELems,'item__link_more', 'item__cut');
 
+        //Apply to effect to 'works' section.
         const elemsScrollAnimate = document.querySelectorAll('.works__column, .section-title');
         AnimateOnScroll(elemsScrollAnimate, false, true);
 
+        //Apply effect to menu and button 'down'.
         const elemsForSmoothScroll = document.querySelectorAll('*[href*="#"]');
         SmoothScroll(elemsForSmoothScroll);
 
+        //Apply effect to 'skills' section.
         const filterButtons = document.querySelectorAll('.filter__item'),
             filterItems = document.querySelectorAll('.skills__item');
         FilterItems(filterButtons,filterItems);
-
+        //Apply effect to 'skills' section on back part of cards.
         const progressElems = document.querySelectorAll('.skills__item');
         AnimateProgressBar(progressElems, 'progress-bar');
+
+        const btnUp = document.querySelector('.btn__round_arrow-up');
+        ScrollTopBtn(btnUp);
         
     }
 }());
